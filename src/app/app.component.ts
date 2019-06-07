4816e48d16d424b9d330e8de85f6c188def27e8c
Thapelo <thapeloseerane1@gmail.com>
2019-06-07 12:13:38 +0200

    Commiting my ToDoListApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Script } from 'vm';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoListApp';
+List=["---Default---"]
+
+TodoList: string=""
+
+addList(){
+ this.List.push(this.TodoList)
+ this.TodoList=""
+}
+
+remove(chores){
+ let index = this.List.indexOf(chores)
+ this.List.splice(index,1)
+}
+editChore(chores){
+  let index= this.List.indexOf(chores)
+  this.List[index] = prompt("Please edit task",chores)
+}
+
+
 }
